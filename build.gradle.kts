2a2d601f0bdbefa150d86487c270632a549cd213
Kanzaji <60540476+Kanzaji@users.noreply.github.com>
2023-09-11 11:31:28 +0200

    Make JEI CompileOnly so the game actually boots
    TODO: Find out why JEI was crashing and how to fix it.

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     minecraft(group = "net.minecraftforge", name = "forge", version = "1.12.2-14.23.5.2855")
     
     implementation(project(":IC2-Patched"))
-    implementation(fg.deobf(group = "mezz.jei", name = "jei_1.12.2", version = versionJEI))
+    compileOnly(fg.deobf(group = "mezz.jei", name = "jei_1.12.2", version = versionJEI))
 }
 
 tasks {
